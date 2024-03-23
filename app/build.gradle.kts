a9c05efbd7a12525774f470a4962cec838988dd8
THenryInGH <teyhenry2010@gmail.com>
2024-03-24 00:56:37 +0800

    main page and wallet page without chart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,5 +43,9 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.core:core-splashscreen:1.0.0")
 
+
+
     implementation ("de.hdodenhof:circleimageview:3.1.0") //for profile picture
 }
+
+
